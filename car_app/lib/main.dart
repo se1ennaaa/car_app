b6e0cfc2cbddbce0764e2c3589f4fb1bc2005cae
Selen <119594724+se1ennaaa@users.noreply.github.com>
2025-01-14 12:23:46 +0300

    init app

---
+++ car_app/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CarApp());
+}
+
+class CarApp extends StatelessWidget {
+  const CarApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
